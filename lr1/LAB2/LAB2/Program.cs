12695d8d7bd273692c5187a46657a8f41cfe11e4
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-10-08 19:40:38 +0300

    вывожу результат

--- lr1/LAB2/LAB2/Program.cs
+++ lr1/LAB2/LAB2/Program.cs
@@ -20,6 +20,25 @@ namespace LAB2
             }
 
             List<int> list = new List<int>();
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] % 7  == 0) // выделяю числа которые кратны 7
+                {
+                    list.Add(Array[i]);
+                }
+            }
+
+            Console.Write("["); //Вывожу результат как [a1, a2, a3, ..., an]
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i]);
+                if (i < list.Count - 1)
+                {
+                    Console.Write(", ");                
+                }
+            }
+            Console.WriteLine("]");
+            Console.ReadKey();
         }
     }
 }

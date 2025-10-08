e6a96ef071bd9fac17aeb8aeea19874b72937003
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-10-08 18:44:16 +0300

    Даю возможность ввести размер массива

---
+++ lr1/LAB1/LAB1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+     class Program
+    {
+        static void Main()
+        {
+            // Вариант 1 Рыжов Арсений 
+            Console.WriteLine("Введите размер массива: ");
+            int Length = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[Length]; /// даю возможность ввести размер массива
+        }
+    }
+}

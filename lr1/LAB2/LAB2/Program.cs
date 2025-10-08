8e619e37aa3de99959dd2c2ff1d40c5c40b8b623
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-10-08 19:11:29 +0300

    Вводим массив с 1000 значениями и создаем генерацию рандомных чисел

---
+++ lr1/LAB2/LAB2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 2 Рыжов Арсений
+            int[] Array = new int[1000];
+            Random rand = new Random();
+
+        }
+    }
+}

412d2b02e1bca47e38a1b7801fa5762e11a1ab90
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-10-11 17:43:10 +0300

    Находим индекс блюд

--- lr2/LAB2Variant6/LAB2Variant6/SearchigModule.cs
+++ lr2/LAB2Variant6/LAB2Variant6/SearchigModule.cs
@@ -6,7 +6,7 @@ namespace LAB2Variant6
 {
     public class SearchingModule
     {
-        static public int FindIndexGenre(string userQuery, string[] сatalog)
+        static public int FindIndexCatalog(string userQuery, string[] сatalog)
         {
             for (int index = 0; index < сatalog.Length; ++index)
             {
@@ -19,7 +19,19 @@ namespace LAB2Variant6
 
             return -1;
         }
+        static public (List<string>, List<int>) FindAllDishsByCenre(int indexCatalog,
+                                                            List<string>[] allDishs,
+                                                            List<int>[] counts)
+        {
+            List<string> dishsByCatalog = new List<string>();
+            List<int> countsByCatalog = new List<int>();
 
-        
+            dishsByCatalog = alldishs[indexCatalog];
+            countsByCatalog = counts[indexCatalog];
+
+            return (dishsByCatalog, countsByCatalog);
+        }
     }
 }
+        
+    

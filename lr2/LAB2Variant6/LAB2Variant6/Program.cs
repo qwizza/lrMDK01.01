c68711544de3502c359d16d61aac852ca60a740e
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-10-11 18:07:40 +0300

    добавил метот для пеечати списка блюд

--- lr2/LAB2Variant6/LAB2Variant6/Program.cs
+++ lr2/LAB2Variant6/LAB2Variant6/Program.cs
@@ -4,6 +4,11 @@ namespace LAB2Variant6
 {
     class Program
     {
+        static void Print(List<string> dishs)
+        {
+            Console.WriteLine(String.Join(", ", dishs));
+        }
+
         static void Main()
         {
             string[] catalog = new string[] { "Суп", "Салат", "Выпечка", "Гарниры" };

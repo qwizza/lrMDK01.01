e8b15d4187986e377b1f8817bf02e347947716c5
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-10-11 17:56:37 +0300

    Сортируем блюда по популярности, а так же расчитываем сренднее значение

---
+++ lr2/LAB2Variant6/LAB2Variant6/AnalysisDataModule.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Security;
+
+namespace LAB2Variant6
+{
+    public class AnalysisDataModule
+    {
+        // сортируем блюда по популярности 
+        static public void SortDishs(List<string> dishs, List<int> counts)
+        {
+            for (int i = 0; i < counts.Count; ++i)
+            {
+                for (int j = 0; j < counts.Count - 1; ++j)
+                {
+                    bool condition = counts[j + 1] > counts[j];
+                    if (condition)
+                    {
+                        int temp_counts = counts[j];
+                        counts[j] = counts[j + 1];
+                        counts[j + 1] = temp_counts;
+
+                        string temp_books = dishs[j];
+                        dishs[j] = dishs[j + 1];
+                        dishs[j + 1] = temp_books;
+                    }
+                }
+            }
+        }
+
+        //расчитываем среднее значение 
+        static public double CalculateAverage(List<int> counts)
+        {
+            int sum = 0;
+            foreach (int count in counts)
+            {
+                sum += count;
+            }
+
+            return sum / counts.Count;
+        }
+    }
+}

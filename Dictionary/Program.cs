376a7b489a57169147953f0dd8d1f7d76178bac1
Kate <20katerina0060@gmail.com>
2025-10-25 12:09:02 +0300

    дописал добавку ключа со своим щначением

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -13,9 +13,9 @@ namespace Dictionary
         static void Main(string[] args)
         {
             Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
-            dictionary.Add("blue", new List<string>() { "шар", "я" });
+            dictionary.Add("blue", new List<string>() { "я" });
             dictionary.Add("green", new List<string>() { "трава", "мяч" });
-            dictionary.Add("red", new List<string>() { "яблоко", "мясо" });
+            dictionary.Add("red", new List<string>() { "яблоко", "мясо", "велосипед" });
         }
     }
 }

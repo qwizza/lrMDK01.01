e0305b49ba0c079c30aeb36a45d0f1c97e9e7453
Kate <20katerina0060@gmail.com>
2025-10-25 12:06:21 +0300

    добавил 3 ключа со своими значениями

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,9 @@ namespace Dictionary
         static void Main(string[] args)
         {
             Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            dictionary.Add("blue", new List<string>() { "шар", "я" });
+            dictionary.Add("green", new List<string>() { "трава", "мяч" });
+            dictionary.Add("red", new List<string>() { "яблоко", "мясо" });
         }
     }
 }

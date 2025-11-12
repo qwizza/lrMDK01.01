8165f9fce5e29d780c0341648f6312aef83e629c
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-12 15:37:56 +0300

    создаем список продаж

---
+++ phone/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Sale> sales = GenerateSalesData(30);
+        }
+    }
+}

c19563977207e98a04b96540b41a194eec4a0b4c
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-13 02:42:06 +0300

    Реализовал класс AnalysisDataModule и в нем сделал фильтрацию продаж по периоду

---
+++ Дистанционное занятие/PhoneSale/PhoneSale/AnalysisDataModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneShop
+{
+    public class AnalysisDataModule
+    {
+        static public List<PhoneSale> GetSalesByPeriod(List<PhoneSale> sales, DateTime startDate, DateTime endDate)
+        {
+            var result = new List<PhoneSale>();
+            foreach (var sale in sales)
+            {
+                if (sale.Date >= startDate && sale.Date <= endDate)
+                    result.Add(sale);
+            }
+            return result;
+        }
+    }
+}

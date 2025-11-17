31d735653affb637db026ee547183cd149f01384
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-17 23:39:36 +0300

    написал структуру своего проекта, в нее входит бренд телефона, модель, неделя продажи, счетчик продаж

--- lr2_new/Variant 12/Variant 12/Phone.cs
+++ lr2_new/Variant 12/Variant 12/Phone.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Variant_12
 {
-    internal class Phone
+    public struct Phone
     {
+        public string brand;
+        public string model;
+        public int week;
+        public int countSell;
     }
 }

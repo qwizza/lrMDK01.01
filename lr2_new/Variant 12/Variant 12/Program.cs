982ba1e733bb51289be78e67ee3e17b3c7cc851c
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-18 00:04:23 +0300

    Добавил по 12 запясей телефоноф марки iPhone, Samsung и Huawei

--- lr2_new/Variant 12/Variant 12/Program.cs
+++ lr2_new/Variant 12/Variant 12/Program.cs
@@ -11,6 +11,45 @@ namespace Variant_12
         static void Main(string[] args)
         {
             List<Phone> sales = new List<Phone>();
+            //iPhone
+            sales.Add(new Phone { brand = "iPhone", model = "17 Pro", week = 1, countSell = 1 });
+            sales.Add(new Phone { brand = "iPhone", model = "17 Pro", week = 2, countSell = 0 });
+            sales.Add(new Phone { brand = "iPhone", model = "17 Pro", week = 3, countSell = 0 });
+            sales.Add(new Phone { brand = "iPhone", model = "17 Pro", week = 4, countSell = 2 });
+            sales.Add(new Phone { brand = "iPhone", model = "16 Pro", week = 1, countSell = 1 });
+            sales.Add(new Phone { brand = "iPhone", model = "16 Pro", week = 2, countSell = 3 });
+            sales.Add(new Phone { brand = "iPhone", model = "16 Pro", week = 3, countSell = 2 });
+            sales.Add(new Phone { brand = "iPhone", model = "16 Pro", week = 4, countSell = 4 });
+            sales.Add(new Phone { brand = "iPhone", model = "15 Pro", week = 1, countSell = 2 });
+            sales.Add(new Phone { brand = "iPhone", model = "15 Pro", week = 2, countSell = 5 });
+            sales.Add(new Phone { brand = "iPhone", model = "15 Pro", week = 3, countSell = 0 });
+            sales.Add(new Phone { brand = "iPhone", model = "15 Pro", week = 4, countSell = 1 });
+            //Samsung
+            sales.Add(new Phone { brand = "Samsung", model = "S25 Ultra", week = 1, countSell = 1 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25 Ultra", week = 2, countSell = 0 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25 Ultra", week = 3, countSell = 3 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25 Ultra", week = 4, countSell = 4 });
+            sales.Add(new Phone { brand = "Samsung", model = "S24 Ultra", week = 1, countSell = 0 });
+            sales.Add(new Phone { brand = "Samsung", model = "S24 Ultra", week = 2, countSell = 0 });
+            sales.Add(new Phone { brand = "Samsung", model = "S24 Ultra", week = 3, countSell = 2 });
+            sales.Add(new Phone { brand = "Samsung", model = "S24 Ultra", week = 4, countSell = 3 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25+", week = 1, countSell = 2 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25+", week = 2, countSell = 0 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25+", week = 3, countSell = 3 });
+            sales.Add(new Phone { brand = "Samsung", model = "S25+", week = 4, countSell = 4 });
+            //Huawei
+            sales.Add(new Phone { brand = "Huawei", model = "P60 Pro", week = 1, countSell = 4 });
+            sales.Add(new Phone { brand = "Huawei", model = "P60 Pro", week = 2, countSell = 3 });
+            sales.Add(new Phone { brand = "Huawei", model = "P60 Pro", week = 3, countSell = 5 });
+            sales.Add(new Phone { brand = "Huawei", model = "P60 Pro", week = 4, countSell = 2 });
+            sales.Add(new Phone { brand = "Huawei", model = "Mate 50", week = 1, countSell = 2 });
+            sales.Add(new Phone { brand = "Huawei", model = "Mate 50", week = 2, countSell = 1 });
+            sales.Add(new Phone { brand = "Huawei", model = "Mate 50", week = 3, countSell = 0 });
+            sales.Add(new Phone { brand = "Huawei", model = "Mate 50", week = 4, countSell = 1 });
+            sales.Add(new Phone { brand = "Huawei", model = "Nova 12", week = 1, countSell = 3 });
+            sales.Add(new Phone { brand = "Huawei", model = "Nova 12", week = 2, countSell = 2 });
+            sales.Add(new Phone { brand = "Huawei", model = "Nova 12", week = 3, countSell = 4 });
+            sales.Add(new Phone { brand = "Huawei", model = "Nova 12", week = 4, countSell = 3 });
         }
     }
 }

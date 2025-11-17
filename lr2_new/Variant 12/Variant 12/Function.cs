ace2bbbd94da5db0c48d2f8b844299bd6442c257
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-18 00:07:28 +0300

    Создал клас Function, в нем я буду реализовывать отчет по продажам

---
+++ lr2_new/Variant 12/Variant 12/Function.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant_12
+{
+    internal class Function
+    {
+    }
+}

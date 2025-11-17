9907b176cd67ff3538222c418c6cc05b423a6b84
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-18 00:25:56 +0300

    Запросил марку телефона и создал два словаря: первый словарь хоанит название модели и общее кол-во продаж; второй словарь хранит название модели, номер недели и продажи за неделю

--- lr2_new/Variant 12/Variant 12/Function.cs
+++ lr2_new/Variant 12/Variant 12/Function.cs
@@ -10,7 +10,12 @@ namespace Variant_12
     {
         public static void ShowTopPhones(List<Phone> phones)
         {
-            
+            Console.WriteLine("Введите марку телефона (iPhone, Samsung, Huawei): ");
+            string brand = Console.ReadLine();
+
+            Dictionary<string, int> totalSales = new Dictionary<string, int>();
+
+            Dictionary<string, Dictionary<int, int>> weeklySales = new Dictionary<string, Dictionary<int, int>>();
         }
     }
 }

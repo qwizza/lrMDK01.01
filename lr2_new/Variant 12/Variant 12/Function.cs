4c225109711ce1e2c2e193b36f757a8f0eb7c62d
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-18 00:43:17 +0300

    Добавил анализ продаж телефонов по брендам и моделям

--- lr2_new/Variant 12/Variant 12/Function.cs
+++ lr2_new/Variant 12/Variant 12/Function.cs
@@ -16,6 +16,30 @@ namespace Variant_12
             Dictionary<string, int> totalSales = new Dictionary<string, int>();
 
             Dictionary<string, int[]> weekSales = new Dictionary<string, int[]>();
+
+            foreach (Phone phone in phones)
+            {
+                if (phone.brand == brand)
+                {
+                    if (modelSales.ContainsKey(phone.model))
+                    {
+                        modelSales[phone.model] += phone.countSell;
+                    }
+                    else
+                    {
+                        modelSales.Add(phone.model, phone.countSell);
+                        weekSales.Add(phone.model, new int[4]);
+                    }
+
+                    weekSales[phone.model][phone.week - 1] = phone.countSell;
+                }
+            }
+
+            if (modelSales.Count == 0)
+            {
+                Console.WriteLine("Такого бренда нету в магазине");
+                return;
+            }
         }
     }
 }

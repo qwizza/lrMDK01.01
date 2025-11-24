dcd2b5865127fdfde0e8b0d3b2909ec64f446188
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-25 01:13:10 +0300

    реализовал конструктор для создания матрицы

--- Дистанционное занятие/matrix/matrix/Matrix.cs
+++ Дистанционное занятие/matrix/matrix/Matrix.cs
@@ -7,5 +7,12 @@ namespace matrix
         private int[,] data; //хранилище чисел матриц
         private int rows; //кол-во строк
         private int columns; //кол-во столбцов 
+
+        public Matrix(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            data = new int[rows, columns];
+        }
     }
 }

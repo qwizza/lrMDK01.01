95c2712632aa28869363b2d3ab1c5fd7a7c151b6
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-25 01:56:37 +0300

    реализую метод для умножени; добавил проверку возможности умножения

--- Дистанционное занятие/matrix/matrix/Program.cs
+++ Дистанционное занятие/matrix/matrix/Program.cs
@@ -4,6 +4,16 @@ namespace matrix
 {
     internal class Program
     {
+        public static Matrix Multiply(Matrix a, Matrix b)
+        {
+            //проверка возможности умножения
+            if (a.GetColumns() != b.GetColumns())
+            {
+                Console.WriteLine("Нельзя умножить, т.к. столбцы первой не равны строки второй");
+                return null;
+            }
+        }
+
         static void Main()
         {
             

1011b0c2a396ae1314425f3d3eaa6d7c6725e596
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-11-25 01:06:43 +0300

    реализую метод для хранения чисел матриц, кол-во строк и столбцов

--- Дистанционное занятие/matrix/matrix/Matrix.cs
+++ Дистанционное занятие/matrix/matrix/Matrix.cs
@@ -4,6 +4,8 @@ namespace matrix
 {
     public class Matrix
     {
-
+        private int[,] data; //хранилище чисел матриц
+        private int rows; //кол-во строк
+        private int columns; //кол-во столбцов 
     }
 }

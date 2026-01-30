8ec72dbda83e01e35b46998c5a46f76ce3b3aa40
Daniil5903 <danilka7518@gmail.com>
2026-01-30 10:37:27 +0300

    Реализовал модель данных для блюда, который содержит nameDish(название блюда), descriptionDish(описание блюда), priceDish(цена), ingredientDish(ингредиенты блюда) и реализовал вывод в ListBox

---
+++ LR3/Dish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3
+{
+    public class Dish
+    {
+        public string nameDish_ { get; set; }
+        public string descriptionDish_ { get; set; }
+        public int priceDish_ { get; set; }
+        public string ingredients_ { get; set; }
+        
+        public Dish(string nameDish, string descriptionDish, int priceDish, string ingredients)
+        {
+            nameDish_ = nameDish;
+            descriptionDish_ = descriptionDish;
+            priceDish_ = priceDish;
+            ingredients_ = ingredients;
+
+        }
+        public override string ToString()
+        {
+            return $"{nameDish_} - {priceDish_}";
+        }
+    }
+}

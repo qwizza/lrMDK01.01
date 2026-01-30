fde9a478d2fdf3210d2c13111917c47a46b84662
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2026-01-30 20:14:46 +0300

    Заполняю списки блюдами, описанием, ингридиентами

--- LR3/MainForm.cs
+++ LR3/MainForm.cs
@@ -24,10 +24,44 @@ namespace LR3
             menuGroups.Add(new MenuGroup("Десерты", "Сладкие блюда"));
             menuGroups.Add(new MenuGroup("Напитки", "Холодные и горячие напитки"));
 
-            MenuListBox.DataSource = menuGroups; 
+            MenuListBox.DataSource = menuGroups;
             MenuListBox.DisplayMember = "name_";
 
             InitializeDishes();
+        }
+        private void InitializeDishes()
+        {
+            var soups = new List<Dish>
+            {
+                new Dish("Борщ", "Красный борщ со сметаной", 250, "Свекла, капуста, мясо, сметана"),
+                new Dish("Куриный суп", "Легкий куриный суп", 200, "Курина, лапша, морковь, лук"),
+                new Dish("Солянка", "Густой мясной суп", 280, "Разные виды мяса, огурцы, оливки")
+            };
+            dishesByGroup["Супы"] = soups;
+
+            var mainDishes = new List<Dish>
+            {
+                new Dish("Плов", "Узбекский плов", 350, "Рис, мясо, морковь, специи"),
+                new Dish("Котлеты с пюре", "Котлеты с картофельным пюре", 300, "Фарш, картофель, лук, молоко"),
+                new Dish("Стейк", "Стейк из говядины", 450, "Говядина, соль, перец, травы")
+            };
+            dishesByGroup["Основные блюда"] = mainDishes;
+
+            var desserts = new List<Dish>
+            {
+                new Dish("Торт Наполеон", "Слоеный торт с кремом", 180, "Тесто, крем, сахарная пудра"),
+                new Dish("Мороженое", "Ванильное мороженое", 120, "Молоко, сахар, ваниль"),
+                new Dish("Чизкейк", "Классический чизкейк", 220, "Сыр, печенье, сливки")
+            };
+            dishesByGroup["Десерты"] = desserts;
+
+            var drinks = new List<Dish>
+            {
+                new Dish("Кофе", "Свежемолотый кофе", 150, "Кофейные зерна, вода"),
+                new Dish("Чай", "Черный чай с лимоном", 100, "Чай, лимон, сахар"),
+                new Dish("Сок", "Апельсиновый сок", 120, "Апельсины")
+            };
+            dishesByGroup["Напитки"] = drinks;
         }
     }
 }

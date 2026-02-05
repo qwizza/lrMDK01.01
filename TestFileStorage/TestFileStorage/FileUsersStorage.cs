2a5284d92ceaae5e0acfc240b6077904aebd7f5d
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2026-02-06 00:50:18 +0300

    пернес программу с флешки и сделал вывод через ComboBox

---
+++ TestFileStorage/TestFileStorage/FileUsersStorage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFileStorage
+{
+    public class FileUsersStorage : IUsersInterface
+    {
+        public List<User> Load()
+        {
+            List<User> result = new List<User>();
+            using (StreamReader reader = new StreamReader("C:\\Users\\zhesmik\\source\\repos\\lrMDK01.01\\TestFileStorage\\TestFileStorage.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] userInfo = line.Split('-');
+                    User user = new User(userInfo[0], userInfo[1]);
+                    result.Add(user);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ TestFileStorage/TestFileStorage/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFileStorage
+{
+    public partial class MainForm : Form
+    {
+        private List<User> users = new List<User>();
+        private FileUsersStorage fileUsersStorage = new FileUsersStorage();
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadUserLogins();
+        }
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (LoginComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите пользователя!");
+                return;
+            }                      
+            string selectedLogin = LoginComboBox.SelectedItem.ToString();
+            User selectedUser = users.Find(u => u.Login == selectedLogin);
+            if (selectedUser != null)
+            {
+                if (PasswordTextBox.Text == selectedUser.Password)
+                {
+                    MessageBox.Show("Пароль введен верно :)");
+                }
+                else
+                {
+                    MessageBox.Show("Пароль введен неверно :(");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден!");
+            }
+        }
+        public void LoadUserLogins()
+        {
+            users = fileUsersStorage.Load();
+            LoginComboBox.Items.Clear();
+            foreach (User user in users)
+            {
+                LoginComboBox.Items.Add(user.Login);
+            }
+            if (LoginComboBox.Items.Count > 0)
+            {
+                LoginComboBox.SelectedIndex = 0;
+            }
+        }
+
+    }
+}

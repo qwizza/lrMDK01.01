c63b2ceb934afdde832d0841c42dc6e5b696a979
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2026-02-07 00:11:36 +0300

    наджо доделать

---
+++ GuessTheNumber/WindowsFormsApp1/GueesTheNumber.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Number
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MessangeBoxNumber = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessangeBoxNumber
+            // 
+            this.MessangeBoxNumber.Location = new System.Drawing.Point(12, 162);
+            this.MessangeBoxNumber.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MessangeBoxNumber.Name = "MessangeBoxNumber";
+            this.MessangeBoxNumber.Size = new System.Drawing.Size(580, 93);
+            this.MessangeBoxNumber.TabIndex = 1;
+            this.MessangeBoxNumber.Text = "";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CalendarTrailingForeColor = System.Drawing.SystemColors.ControlText;
+            this.dateTimePicker.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(87, 21);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker.TabIndex = 2;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(83, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите число от 1 до 100:";
+            // 
+            // Text
+            // 
+            this.Text.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text.Location = new System.Drawing.Point(315, 69);
+            this.Text.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(131, 29);
+            this.Text.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timerLabel.Location = new System.Drawing.Point(299, 27);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(163, 21);
+            this.timerLabel.TabIndex = 5;
+            this.timerLabel.Text = " - обратный отсчет";
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CheckButton.Location = new System.Drawing.Point(315, 106);
+            this.CheckButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(131, 48);
+            this.CheckButton.TabIndex = 6;
+            this.CheckButton.Text = "Проверисть";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.Check);
+            // 
+            // Number
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 279);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.MessangeBoxNumber);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox MessangeBoxNumber;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button CheckButton;
+    }
+}
+
---
+++ GuessTheNumber/WindowsFormsApp1/GueesTheNumber.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Number : Form
+    {
+        int timeLeft = 30; 
+        Timer timer;
+        int NextRun_;
+        public Number()
+        {
+            InitializeComponent();
+
+            dateTimePicker.Value = DateTime.Today.AddSeconds(30);
+
+            
+            timer = new Timer();
+            timer.Interval = 1000; 
+            timer.Tick += dateTimePicker_ValueChanged;
+            timer.Start();
+
+            Random random = new Random();
+            NextRun_ = random.Next(1, 101);
+        }
+
+        private void Check(object sender, EventArgs e)
+        {
+            int anInteger = Convert.ToInt32(Text.Text);
+
+            if (NextRun_ > anInteger)
+            {
+                MessageBox.Show("Введеное число меньше!");
+            }
+            else if (NextRun_ < anInteger) 
+            {
+                MessageBox.Show("Введеное число больше!");
+            }
+            else
+            {
+                MessageBox.Show("Вы угадали!");
+            }
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            timeLeft--; 
+
+            dateTimePicker.Value = DateTime.Today.AddSeconds(timeLeft);
+
+            if (timeLeft <= 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Время вышло!");
+            }
+        }
+    }
+}

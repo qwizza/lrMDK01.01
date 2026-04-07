1e2b1fdea8d9ee6dd4c528fc51c0b11c5578214f
qwizza <meowmeowpiupiu@mail.ru>
2026-04-07 11:23:15 +0300

    zachet

---
+++ zachet/Olenivod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zachet
+{
+    public class Olenivod
+    {
+        private string name_;
+        private int age_;
+        private int call_;
+
+        public Olenivod (string name, int age, int call)
+        {
+            name_ = name;
+            age_ = age;
+            call_ = call;
+        }
+        public string GetName()
+        {
+            return name_;
+        }
+    }
+
+}
---
+++ zachet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zachet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Olenivod olenivod = new Olenivod("Иванов", 20, 200000);
+            Olenivod olenivod2 = new Olenivod("Сидоров", 19, 150000);
+            Olenivod olenivod3 = new Olenivod("Петров", 30, 300000);
+            Console.WriteLine($"Имя оленивода:{olenivod.GetName()}");
+
+            List<Olenivod> olenivods = new List<Olenivod>();
+            olenivods.Add(olenivod);
+            olenivods.Add(olenivod2);
+            olenivods.Add(olenivod3);
+            Console.WriteLine(olenivod2.GetName());
+
+        }
+        
+    }
+}
